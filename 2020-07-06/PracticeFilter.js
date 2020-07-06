ed92a4bf6a306088a068e64194f53ce7a2ba2cdb
Ryan <ryanpulu@outlook.com>
2020-07-06 23:44:07 +0800

    0704 & 0706 work

--- 2020-07-06/PracticeFilter.js
+++ 2020-07-06/PracticeFilter.js
@@ -1,5 +1,5 @@
 var arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 15];
-var r = arr.filter(function (x) {
-    return x % 2 !== 0;
+var r = arr.filter(function (x) {           // 过滤, 函数返回值是一个 bool 类型
+    return x % 2 !== 0;         // java ==, !=  //javaScript: == 判断值，不判断类型，比如：2 == '2' 是成立的  2 === '2' 不成立
 });
 console.log(r);
